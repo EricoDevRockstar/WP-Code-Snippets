7bcb8e120f7b886598035f13dd77abb38961fa44
Eric M. W <ericmwangi0916@gmail.com>
2022-12-16 21:37:49 +0300

    Commit the changes

--- WPCodeSnippets.php
+++ WPCodeSnippets.php
@@ -1,5 +1,19 @@
 <?php
-while(have_posts()) {
-    the_post();
-}
-?>
+// GENERAL WP POST LOOP
+if ( have_posts() ) {
+	while ( have_posts() ) {
+		the_post(); 
+		//
+		// Post Content here
+		//
+	} // end while
+} // end if
+
+
+// OR THIS ONE TOO --> The loop starts here
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post();
+
+// and ends here
+<?php endwhile; else : ?>
+	<p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
+<?php endif;
